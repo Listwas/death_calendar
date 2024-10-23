18dcaabf456ec88b1944e5465f72f0322e541f00
listwas <listwa.patrykkr@gmail.com>
2024-10-23 20:28:07 +0200

    changed box display mechanics

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-container1 = document.getElementById("items1");
-container2 = document.getElementById("items2");
 
-function load_boxes(){
-    for (let i = 0; i < 52; i++) {
-        const label = document.createElement("label");
-        const box = document.createElement("input");
-        const span = document.createElement("span");
-        label.classList.add("label");
-        span.classList.add("span")
-        box.type = "checkbox";
+function create_layout() {
 
-        label.appendChild(box)
-        label.appendChild(span)
-        if (i <= 25) {
-            container1.appendChild(label)
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    for (let i = 0; i < 2; i++) {
+        const items = document.createElement('div');
+        items.classList.add(`items${i}`)
+        document.body.appendChild(main)
+        main.appendChild(items);
+
+        for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 26; i++) {
+                const label = document.createElement("label");
+                const box = document.createElement("input");
+                const span = document.createElement("span");
+                label.classList.add("label");
+                span.classList.add("span")
+                box.type = "checkbox";
+        
+                label.appendChild(box)
+                label.appendChild(span)
+                if (i <= 25) {
+                    items.appendChild(label)
+                    
+                } else {items.appendChild(label)}
+            }
+        }
+        
+    }
             
-        } else {container2.appendChild(label)}
-    }
+    const hr = document.createElement('hr');
+    document.body.appendChild(hr)
+    
 }
 
-function ten_years() {
-    for (let i = 0; i < 10; i++) {
-        load_boxes()
+create_layout()
 
-    }
-}
 
-ten_years()
