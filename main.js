7714c2e0157f9fcdef00847a17eaff7fc49f2594
listwas <listwa.patrykkr@gmail.com>
2024-10-23 20:36:35 +0200

    added comments

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 
 function create_layout() {
-
+    // create main element to contain the layout
     const main = document.createElement('main');
     main.classList.add('main');
 
+    // loop to create two boxes 26x10 (one year have 52 weeks)
     for (let i = 0; i < 2; i++) {
         const items = document.createElement('div');
         items.classList.add(`items${i}`)
         document.body.appendChild(main)
         main.appendChild(items);
 
+        // create boxes equvalent to ten years
         for (let i = 0; i < 10; i++) {
             for (let i = 0; i < 26; i++) {
                 const label = document.createElement("label");
