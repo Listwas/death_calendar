5301b9b54f3d567ec2a387d5c2894551f360c9e4
listwas <listwa.patrykkr@gmail.com>
2024-10-24 14:25:53 +0200

    calculating weeks

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
 
-
-function birthday() {
-    const birthday = document.getElementById("birthday").value;
-    localStorage.setItem("brithday", birthday)
-    console.log(birthday)
-}
-
 function create_layout() {
     // create main element to contain the layout
     const main = document.createElement('main');
@@ -14,20 +7,20 @@ function create_layout() {
     // loop to create two boxes 26x10 (one year have 52 weeks)
     for (let i = 0; i < 2; i++) {
         const items = document.createElement('div');
-        items.classList.add(`items${i}`)
-        document.body.appendChild(main)
+        items.classList.add(`items${i}`);
+        document.body.appendChild(main);
         main.appendChild(items);
 
         // create boxes equvalent to ten years
         for (let i = 0; i < 10; i++) {
             for (let i = 0; i < 26; i++) {
                 const div = document.createElement('div');
-                div.classList.add("box_element")
+                div.classList.add("box_element");
                 // append 26 boxes each item container
                 if (i <= 25) {
-                    items.appendChild(div)
+                    items.appendChild(div);
                     
-                } else {items.appendChild(div)}
+                } else {items.appendChild(div);}
             }
         }
         
@@ -35,7 +28,7 @@ function create_layout() {
 
     // place separator on each ten years
     const hr = document.createElement('hr');
-    document.body.appendChild(hr)
+    document.body.appendChild(hr);
     
 }
 
@@ -43,15 +36,32 @@ function generate_by_years() {
     // get birthday and years from local storage
     const years = localStorage.getItem("years");
     if (!years || years === undefined) {
-        window.location = "/birthday.html"
+        window.location = "/birthday.html";
     } 
 
-    const real_years = years.split(0, 1)
+    const real_years = years.split(0, 1);
     
 
     for (let i = 0; i < real_years; i++) {
-        create_layout()
+        create_layout();
     } 
+}
+
+function birthday() {
+    const date = new Date();
+    
+    const birthday = new Date(document.getElementById("birthday").value);
+
+    const diff_in_months = date - birthday;
+    const diff_in_days = diff_in_months / (1000 * 60 * 60 * 24);
+    const diff_in_weeks = diff_in_days / 7;
+    
+    console.log(diff_in_weeks)
+    localStorage.setItem("brithday", diff_in_weeks);
+    if (!diff_in_weeks || diff_in_weeks <= 0) {
+        alert("set your birthday!");
+        window.location = "/birthday.html";
+    } else {window.location = "/how_long.html";}
 }
 
 function modify_years(years) {
@@ -77,3 +87,5 @@ function modify_years(years) {
 
     element.innerText = int_el; 
 }
+
+
